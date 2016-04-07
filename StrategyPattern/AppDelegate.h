b27b4fa74285356eb1d838748f47f5e9b9e74917
Ross <954777574@qq.com>
2016-04-07 15:34:49 +0800

    Initial Commit

---
+++ StrategyPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StrategyPattern
+//
+//  Created by LV on 16/4/7.
+//  Copyright © 2016年 Wieye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
